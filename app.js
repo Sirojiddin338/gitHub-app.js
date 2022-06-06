f632476bd506b584b46ef90dcf89242f93370195
Sirojiddin <sirojiddin.sattorov93gmail.com>
2022-06-06 10:12:01 +0500

    first Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// let user = +prompt('Your are age: ', '');
+// console.log(typeof user);
+
+let user = 'Sirojiddin';
+console.log(user);
+
+let userAge = 28;
+console.log(userAge);
+
+console.log(`mani ismim ${user} va mening yoshim ${userAge} , hozirda ishsizman!`)
+
+// const ism = prompt('Sizning Ismingiz: ', '');
+// console.log(`Mening Ismim ${ism} ...!`);
+
+
+
+
+
+
+
+
+
+
+
+// 10- dars;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
